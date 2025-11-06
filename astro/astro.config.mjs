88da45661b3c8cd2cd6ee21656d5c771c12dd4f5
nick-sohl <nicksohl59@gmail.com>
2025-11-06 17:55:47 +0100

    add tailwind to astro config

--- astro/astro.config.mjs
+++ astro/astro.config.mjs
@@ -3,3 +3,8 @@ import tailwindcss from "@tailwindcss/vite";
 
 // https://astro.build/config
 export default defineConfig({});
+export default defineConfig({
+    plugins: [tailwindcss()],
+    }
+  }, 
+});
